8b68d45b202cea7cf8b4b0fbc4a3186e4301d552
Gyulai Laci <lacfy14@gmail.com>
2023-03-23 19:16:58 +0100

    dolgozat minta

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const doc = {
+   tbody: document.querySelector('#tbody')
+
+};
+
+const state = {
+    host:'http://localhost:8000/',
+    todos: []
+};
+
+getTodos();
+function getTodos() {
+    let url = state.host + 'todos';
+    fetch(url)
+    .then(Response => Response.json())
+    .then(result => {
+        state.todos = result;
+        render();
+    });
+    
+}
+
+function render() {
+    let rows = '';
+    state.todos.forEach((todo) => {
+        
+         rows += `
+         <tr>
+             <td>${todo.id}</td>
+             <td>${todo.name}</td>
+             <td>${todo.ready}</td>
+         </tr>
+         `;
+         
+    } );
+    doc.tbody.innerHTML = rows;
+}
